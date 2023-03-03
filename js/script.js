5ba1e8be8c2d0a5fd75711db846fe88314453123
Eric Malley <emalley998@gmail.com>
2023-03-02 19:03:42 -0500

    route to tv shows

--- js/script.js
+++ js/script.js
@@ -27,16 +27,52 @@ async function displayPopularMovies() {
   })
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular')
+  console.log(results)
+  results.forEach((show) => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+
+    div.innerHTML = `
+    <a href="tv-deatils.html?id=${show.id}">
+      ${
+        show.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="show-title">`
+          : `<img src="../images/no-image.jpg" class="card-img-top" alt="${show.name}">`
+      }
+    </a>
+    <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+        </p>
+    </div>`
+    document.querySelector('#popular-shows').appendChild(div)
+  })
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show')
+}
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show')
+}
+
 //Fetch Data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = '02612e7718e87ae931ad313a94941d1e'
   const API_URL = 'https://api.themoviedb.org/3/'
+
+  showSpinner()
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   )
 
   const data = await response.json()
+  hideSpinner()
+
   return data
 }
 
@@ -58,6 +94,7 @@ function init() {
       break
     case '/shows.html':
       console.log('shows')
+      displayPopularShows()
       break
     case '/movies-details.html':
       console.log('movie details')
